fb0f0b5b2c8bd27f90341ec31bda341a9a48c339
cahangir <cahangir.com>
2025-08-26 13:42:27 +0400

    test1

--- _printf.c
+++ _printf.c
@@ -1,18 +1,61 @@
 #include "main.h"
 
 /**
+ * print_char - prints a single character
+ * @args: va_list containing the character to print
+ * Return: number of characters printed
+ */
+int print_char(va_list args)
+{
+	char c = (char)va_arg(args, int);
+
+	write(1, &c, 1);
+	return (1);
+}
+
+/**
+ * print_string - prints a string
+ * @args: va_list containing the string to print
+ * Return: number of characters printed
+ */
+int print_string(va_list args)
+{
+	int count = 0;
+	char *str = va_arg(args, char *);
+
+	if (!str)
+		str = "(null)";
+
+	while (*str)
+	{
+		write(1, str++, 1);
+		count++;
+	}
+
+	return (count);
+}
+
+/**
+ * print_percent - prints a percent sign
+ * Return: number of characters printed
+ */
+int print_percent(void)
+{
+	write(1, "%", 1);
+	return (1);
+}
+
+/**
  * _printf - produces output according to a format
- * @format: format string containing the characters and the specifiers
- *
+ * @format: format string
  * Return: number of characters printed (excluding null byte)
  */
 int _printf(const char *format, ...)
 {
 	va_list args;
 	int i = 0, count = 0;
-	char *str;
 
-	if (format == NULL)
+	if (!format)
 		return (-1);
 
 	va_start(args, format);
@@ -28,29 +71,11 @@ int _printf(const char *format, ...)
 				return (-1);
 			}
 			if (format[i] == 'c')
-			{
-				char c = (char)va_arg(args, int);
-
-				write(1, &c, 1);
-				count++;
-			}
+				count += print_char(args);
 			else if (format[i] == 's')
-			{
-				str = va_arg(args, char *);
-				if (str == NULL)
-					str = "(null)";
-				while (*str)
-				{
-					write(1, str, 1);
-					str++;
-					count++;
-				}
-			}
+				count += print_string(args);
 			else if (format[i] == '%')
-			{
-				write(1, "%", 1);
-				count++;
-			}
+				count += print_percent();
 			else
 			{
 				write(1, "%", 1);
