503afe411d3878aff9f0e6e07c2c8008847b598b
Your Name <you@example.com>
2025-08-30 12:29:50 +0400

    test

--- main.h
+++ main.h
@@ -1,22 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdarg.h>
-#include <unistd.h>
-
+/* _printf function prototype */
 int _printf(const char *format, ...);
-//int print_char(va_list args);
-//int print_string(va_list args);
-int print_percent(void);
-int handle_format(const char *format, va_list args, int *i);
-int parse_format(const char *format, va_list args);
 
-
-int print_integer(int n);
-
+/* Helper functions prototypes */
 int print_char(char c);
 int print_string(char *s);
 int print_int(int n);
-
 
 #endif /* MAIN_H */
